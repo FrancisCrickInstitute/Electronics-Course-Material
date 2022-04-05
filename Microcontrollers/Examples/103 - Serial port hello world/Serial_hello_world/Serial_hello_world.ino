d477a08f9a1244190db3bf2bd5165a867a78b638
XaviGithubCF <xavier.cano-ferrer@crick.ac.uk>
2022-04-05 12:45:41 +0100

    Create Serial_hello_world.ino

---
+++ Microcontrollers/Examples/103 - Serial port hello world/Serial_hello_world/Serial_hello_world.ino
@@ -0,0 +1,14 @@
+
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello Making Electronics Course 22 Class");
+  delay(50);        // delay in between reads for stability
+}
